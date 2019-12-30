6b52da137730a4d20c058342fd031106bb4c3ae0
Kristof Buts <github@kristofbuts.be>
2019-12-30 13:55:46 +0100

    Basic setup

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,87 @@
+<template>
+	<q-layout view="hHh lpr fff">
+		<q-header class="bg-primary text-white" height-hint="98">
+			<q-toolbar>
+				<q-btn
+					dense
+					flat
+					round
+					icon="menu"
+					@click="drawerOpen = !drawerOpen"
+				/>
+
+				<q-toolbar-title>
+					UI Clinic
+				</q-toolbar-title>
+			</q-toolbar>
+		</q-header>
+
+		<q-drawer v-model="drawerOpen" side="left" overlay behavior="mobile">
+			<q-scroll-area
+				style="height: calc(100% - 192px); margin-top: 192px; border-right: 1px solid #ddd"
+			>
+				<q-list>
+<!--					<q-item-label header>Navigation</q-item-label>-->
+					<q-item
+						v-for="nav in navigation"
+						:key="nav.label"
+						:to="nav.to"
+						clickable
+						exact
+					>
+						<q-item-section avatar>
+							<q-icon :name="nav.icon" />
+						</q-item-section>
+						<q-item-section>
+							<q-item-label>{{ nav.label }}</q-item-label>
+						</q-item-section>
+					</q-item>
+				</q-list>
+			</q-scroll-area>
+
+			<q-img
+				class="absolute-top"
+				src="statics/mountains.jpg"
+				style="height: 192px"
+			>
+				<div class="absolute-bottom bg-transparent">
+					<q-avatar size="56px" class="q-mb-sm">
+						<img
+							src="https://randomuser.me/api/portraits/lego/0.jpg"
+						/>
+					</q-avatar>
+					<div class="text-weight-bold">Kristof Buts</div>
+					<div>@butskristof</div>
+				</div>
+			</q-img>
+		</q-drawer>
+
+		<q-page-container>
+			<router-view />
+		</q-page-container>
+	</q-layout>
+</template>
+
+<script>
+export default {
+	name: "Layout",
+
+	data() {
+		return {
+			drawerOpen: false,
+			navigation: [
+				{
+					label: "App",
+					icon: "list",
+					to: "/"
+				},
+				{
+					label: "Settings",
+					icon: "settings",
+					to: "/"
+				}
+			]
+		};
+	}
+};
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,9 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
-  </q-page>
+	<p>UI Clinic</p>
 </template>
 
 <script>
 export default {
-  name: "PageIndex"
+	name: "PageIndex"
 };
 </script>
